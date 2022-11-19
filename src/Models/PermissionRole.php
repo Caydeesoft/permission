81a8fc2d2444ffd24e68b8fd0f69646356b58e51
Dennis Kiptugen <dennis@Denniss-MacBook-Pro.local>
2022-11-19 21:46:45 +0300

    fix blade options

--- src/Models/PermissionRole.php
+++ src/Models/PermissionRole.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PermissionRole extends Model
     {
-        use HasFactory;
+       
         public $timestamps = FALSE;
         public function role()
             {
