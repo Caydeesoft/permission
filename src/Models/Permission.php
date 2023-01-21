77d3d86043a3fb998ea90aa09460257415ba75b6
Dennis Kiptugen <dennis@Denniss-MacBook-Pro.local>
2023-01-21 22:12:30 +0300

    access level

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -6,14 +6,16 @@ use Caydeesoft\Permission\Casts\JsonCast;
 use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
-{
-    public $timestamps = false;
-
-    protected $fillable = ['name','access_level', 'action'];
-    protected $casts = ['access_level'=>JsonCast::class]
-
-    public function roles()
     {
-        return $this->belongsToMany(Role::class);
+        public $timestamps = false;
+
+        protected $fillable = ['name','access_level', 'action'];
+        protected $casts =  [
+                                'access_level'=>JsonCast::class
+                            ];
+
+        public function roles()
+            {
+                return $this->belongsToMany(Role::class);
+            }
     }
-}
